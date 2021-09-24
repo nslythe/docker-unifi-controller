b0f7a2b65a5124d491f96b40493c735bb838e39d
Nicolas Slythe <nicolas@slythe.net>
2021-09-24 05:47:24 +0000

    init

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM base
+
+ARG UNIFI_VERSION=6.4.54
+
+RUN apt-get update && apt-get install -y openjdk-8-jre-headless jsvc mongodb-server curl logrotate
+
+ADD https://dl.ui.com/unifi/${UNIFI_VERSION}/unifi_sysvinit_all.deb /unifi.dep
+
+RUN dpkg -i /unifi.dep
+
+EXPOSE 3478/udp
+EXPOSE 10001/udp
+EXPOSE 8080
+EXPOSE 8443
+EXPOSE 1900/udp
+EXPOSE 8843
+EXPOSE 8880
+EXPOSE 6789
+EXPOSE 5514/udp
+
+VOLUME /config
+
+COPY root /
---
+++ root/run.py
@@ -0,0 +1,56 @@
+
+import os
+import subprocess
+
+class Run(object):
+    def __init__(self):
+        self.process = None
+
+    def description(self):
+        return [
+            "Docker base image"
+        ]
+
+    def config(self):
+        os.makedirs("/config/data", mode=0o777, exist_ok=True)
+        os.makedirs("/config/logs", mode=0o777, exist_ok=True)
+        os.makedirs("/config/run", mode=0o777, exist_ok=True)
+        
+        if not os.path.isdir("/usr/lib/unifi/data") and not os.path.islink("/usr/lib/unifi/data"):
+            os.symlink("/config/data", "/usr/lib/unifi/data")
+        if not os.path.isdir("/usr/lib/unifi/logs") and not os.path.islink("/usr/lib/unifi/logs"):
+            os.symlink("/config/logs", "/usr/lib/unifi/logs")
+        if not os.path.isdir("/usr/lib/unifi/run") and not os.path.islink("/usr/lib/unifi/run"):
+            os.symlink("/config/run", "/usr/lib/unifi/run")
+
+    def run(self):
+        self.process = subprocess.Popen(
+            args = [
+                "java",
+                "-jar", "/usr/lib/unifi/lib/ace.jar",
+                "start",
+                "-pidfile /var/run/unifi.pid",
+                "-Dfile.encoding=UTF-8",
+                "-Djava.awt.headless=true",
+                "-Dapple.awt.UIElement=true",
+                "-Dunifi.core.enabled=false",
+                "-Xmx1024M",
+                "-XX:+ExitOnOutOfMemoryError",
+                "-XX:+CrashOnOutOfMemoryError",
+                "-XX:ErrorFile=/usr/lib/unifi/logs/hs_err_pid%p.log",
+                "-Dunifi.datadir=/usr/lib/unifi/data",
+                "-Dunifi.logdir=/usr/lib/unifi/logs",
+                "-Dunifi.rundir=/var/run/unifi",
+                "-Dunifi.core.enabled=false",
+                "-procname unif",
+                "-outfile SYSLOG",
+                "-errfile SYSLOG"
+            ],
+            cwd = "/config"
+        )
+        return self.process.wait()
+
+    def stop(self):
+        self.process.kill()
+
+
